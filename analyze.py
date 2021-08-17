2247fd43259fd404d996786b26e26e20da0bb6a4
ebijun1007@gmail.com <ebijun1007@gmail.com>
2021-08-18 02:30:41 +0900

    update model

--- analyze.py
+++ analyze.py
@@ -5,8 +5,19 @@ import numpy as np
 
 df = pd.read_csv('merged.csv')
 roi = df["prediction_roi"].sum()
-df = df.drop(columns=['prediction_roi', 'predict',
-             'time_stamp', 'predict', 'prediction_roi'])
+df = df[[
+    'winner',
+    'player1_age',
+    'player1_year_surface_win',
+    'player1_year_surface_lose',
+    'player1_odds',
+    'player1_H2H',
+    'player2_age',
+    'player2_year_surface_win',
+    'player2_year_surface_lose',
+    'player2_odds',
+    'player2_H2H',
+]]
 df = df.dropna()
 
 x = pd.get_dummies(df[[
@@ -25,7 +36,7 @@ x = pd.get_dummies(df[[
 y = df['winner']  # 目的変数
 
 X_train, X_test, y_train, y_test = train_test_split(
-    x, y, train_size=0.6, random_state=0)
+    x, y, train_size=0.7, random_state=0)
 
 # 定数項(y切片)を必要とする線形回帰のモデル式ならば必須
 X = sm.add_constant(x)
