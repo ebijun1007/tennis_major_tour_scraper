f734dbd5cd6ee08435b0a3ea390ed7d2cc233dbe
ebijun1007@gmail.com <ebijun1007@gmail.com>
2021-08-24 10:56:28 +0900

    fix: answer crawler

--- tennis_explorer/spiders/answer.py
+++ tennis_explorer/spiders/answer.py
@@ -35,32 +35,29 @@ class AnswerExplorer(scrapy.Spider):
             return
 
         match_id = int(response.url.split('id=')[1])
-        print(f"match_id: {match_id}")
-        data = self.df.loc[self.df['match_id'] == match_id]
-        print(data)
-        # data = df.loc[df['match_id'] == match_id]
+        data = self.df.loc[[match_id]]
 
-        winner_name = self.name_order(
-            response.css('th.plName ::text').get())
+        winner_name = self.name_order(response.css('th.plName ::text').get())
 
         if(data.iloc[0]['player1_name'] == winner_name):
             winner = 1
         else:
             winner = 2
-        data.assign(winner=-winner)
-
-        if(round(data.iloc[0]['predict'], 0) == winner):
-            data["prediction_roi"] = data[f"player{winner}_odds"]
+        data = data.assign(winner=winner)
+        predicted_winner = round(data.iloc[0]['predict'], 0)
+        if(predicted_winner == winner):
+            roi = data.iloc[0][f"player{winner}_odds"]
+            data = data.assign(prediction_roi=roi)
         else:
-            data.assign(prediction_roi=-1)
+            data = data.assign(prediction_roi=-1)
 
         with open(self.answer_file, 'a', newline='') as csvfile:
             if csvfile.tell() == 0:
-                data.to_csv(self.answer_file, mode='a',
-                            header=False, index=True)
+                data.to_csv(self.answer_file, mode='w',
+                            header=True, index=True)
             else:
                 data.to_csv(self.answer_file, mode='a',
-                            header=False, index=False)
+                            header=False, index=True)
 
     def name_order(self, name):
         if(len(ordered_name := name.split(" ")) == 2):
