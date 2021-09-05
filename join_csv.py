f53f8812f3a67ffe296ae857f70ddf3f852aef44
ebijun1007 <ebijun1007@gmail.com>
2021-09-05 10:58:46 +0900

    update data

--- join_csv.py
+++ join_csv.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 path = "./data/"
 
-all_files = glob.glob(os.path.join(path, "*.csv"))
+all_files = glob.glob(os.path.join(path, "20*.csv"))
 df_from_each_file = (pd.read_csv(f, sep=',') for f in all_files)
 df_merged = pd.concat(df_from_each_file, ignore_index=True).replace("-", "")
 df_merged.drop_duplicates().to_csv("merged.csv", index=False)
