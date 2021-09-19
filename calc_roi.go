d648c6a172ecd5918286f832c2adb5123dcbbb2a
ebijun1007@gmail.com <ebijun1007@gmail.com>
2021-09-19 14:35:37 +0900

    add roi calculator

---
+++ calc_roi.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func Scrape(url string) {
+	baseUrl := "http://www.tennisexplorer.com/"
+	res := get(url)
+	// defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("#center > div:nth-child(3) > table > thead a").Each(func(i int, s *goquery.Selection) {
+		player_path, ok := s.Attr("href")
+		if ok {
+			fmt.Printf("Review %d: %s\n", i, player_path)
+			calc_roi(baseUrl + player_path)
+		}
+	})
+}
+
+func calc_roi(url string) {
+	res := get(url)
+	// defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	balance := 1.0
+
+	doc.Find("#matches-2021-1-data tbody tr").Each(func(i int, s *goquery.Selection) {
+		if s.HasClass("head") {
+			return
+		}
+		odds, err := strconv.ParseFloat(s.Find("td.course").First().Text(), 64)
+		if err != nil {
+			return
+		}
+
+		balance -= 1
+		win := s.Find("a").First().HasClass("notU")
+		if win {
+			balance += odds
+		}
+
+	})
+	fmt.Printf("%f\n", balance)
+
+}
+
+func get(url string) (res *http.Response) {
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+
+	client := new(http.Client)
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func main() {
+	url := os.Args[1]
+	Scrape(url)
+}
