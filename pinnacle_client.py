d59a2f74e4c91b5d5e7367a02049c6f8656cc5f1
ebijun1007@gmail.com <ebijun1007@gmail.com>
2022-03-29 00:45:35 +0900

    add: error handling

--- pinnacle_client.py
+++ pinnacle_client.py
@@ -17,38 +17,50 @@ class PinnacleClient():
     TODAY = datetime.datetime.today()
     MATCH_FILE = "./data/next_48_hours_match.csv"
     api = pinnacle.APIClient(USERNAME, PASSWORD)
+    count = 0
+    error_count = 0
 
     def execute(self):
         self.load_matches()
         bet_list = self.api.betting.get_bets(
             from_date=self.TODAY, to_date=self.TODAY, betlist=pinnacle.enums.BetListType.Running.value)
         for match in self.matches:
-            tour = match["tour"]
-            if tour != "wta":
+            try:
+                tour = match["tour"]
+                if tour != "wta":
+                    continue
+                home = match["player1_name"]
+                away = match["player2_name"]
+                predict = int(match["predict"])
+                league_id, event_id = self.search_event(home, away)
+                line = self.get_line(league_id, event_id, f"Team{predict}")
+                if(line["price"] < self.MINIMUM_ODDS):
+                    continue
+                if not any([bet for bet in bet_list["straightBets"] if bet['eventId'] == event_id]):
+                    bet = self.place_bet(
+                        line, event_id, f"TEAM{predict}")
+                    print(bet)
+                    self.count += 1
+                    self.SLACK.notify(text=str(bet))
+            except Exception as e:
+                print(tour, home, away, predict)
+                print(league_id, event_id, f"Team{predict}")
+                print(line)
+                print(e)
+                self.error_count += 1
                 continue
-            home = match["player1_name"]
-            away = match["player2_name"]
-            predict = int(match["predict"])
-            print(tour, home, away, predict)
-            league_id, event_id = self.search_event(home, away)
-            line = self.get_line(league_id, event_id, f"Team{predict}")
-            if(line["price"] < self.MINIMUM_ODDS):
-                continue
-            if not any([bet for bet in bet_list["straightBets"] if bet['eventId'] == event_id]):
-                bet = self.place_bet(
-                    line, event_id, f"TEAM{predict}")
-                print(bet)
-                self.SLACK.notify(text=str(bet))
+        self.SLACK.notify(
+            text=f"count: {self.count}, error: {self.error_count}")
 
     def load_matches(self):
         with open(self.MATCH_FILE) as f:
             reader = csv.reader(f, skipinitialspace=True)
             header = next(reader)
             self.matches = [dict(zip(header, row)) for row in reader]
+        self.tennis_events = self.api.market_data.get_fixtures(self.TENNIS_ID)
 
     def search_event(self, home, away):
-        tennis_events = self.api.market_data.get_fixtures(self.TENNIS_ID)
-        for league in tennis_events["league"]:
+        for league in self.tennis_events["league"]:
             if any(word in league["name"] for word in self.IGNORE_WORD):
                 continue
             for event in league["events"]:
