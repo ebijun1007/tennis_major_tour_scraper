f3091e9f2bd7870090fe1b2494a60033f77e6090
蝦名潤 <ebijun@ebinajunnoMacBook.local>
2022-04-13 06:27:00 +0900

    get tour name list

--- tennis_explorer/spiders/odds_history.py
+++ tennis_explorer/spiders/odds_history.py
@@ -8,10 +8,10 @@ import numpy as np
 
 class OddsHistoryExplorer(scrapy.Spider):
     name = "odds_history"
-    home_page = "https://www.oddsportal.com/tennis/results/"
+    HOME_PAGE = "https://www.oddsportal.com/tennis/results/"
     jst = timezone(timedelta(hours=9), 'JST')
     now = datetime.now(jst) - timedelta(days=1)
-    EXCLUDE_WORDS = ["Challenger", "Doubles", "ITF", "Exhibition"]
+    EXCLUDE_WORDS = ["Challenger", "Doubles", "ITF", "Exhibition", "Cup", "Boys", "Girls"]
 
     # Tour
     # name, surface, country, year, prize, point
@@ -22,12 +22,20 @@ class OddsHistoryExplorer(scrapy.Spider):
     # Match
     # tour_id, timestamp, player1_id, player2_id, player1_odds, player2_odds, winner
 
+    def start_requests(self):
+        yield scrapy.Request(url=self.HOME_PAGE, callback=self.parse)
+
+
     def parse(self, response):
         table = response.css("table.table-main.sport")
         for td in table.css('td'):
             name = td.css("a::text").get()
+            if not name:
+                continue
+            if any(element in name for element in self.EXCLUDE_WORDS):
+                continue
             href = td.css("a::attr('href')").get()
-            print(name, href)
+            print(name, response.urljoin(href))
 
             # yield scrapy.Request(url=response.urljoin(href), callback=self.parse_detail)
 
